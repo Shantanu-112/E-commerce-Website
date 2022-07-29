545ff6991405426f6a7fd90461439d852417ef8f
Shantanu-112 <110170382+Shantanu-112@users.noreply.github.com>
2022-07-30 02:04:25 +0530

    Initial commit

---
+++ ecommerce-website-fashion-wear/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Components/Header';
+import About from './Components/About';
+import Product from './Components/Product';
+import Footer from './Components/Footer';
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <About/>
+      <Product/>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
